c1c3d3a1a5cd869f3183ec2ad8f6b5662a0ca0d9
Pieter van Ginkel <pginkel@gmt.nl>
2014-03-27 03:19:06 +0100

    Made the DbAware annotation inherited.

--- gmtdata/src/main/java/nl/gmt/data/struts/DbAware.java
+++ gmtdata/src/main/java/nl/gmt/data/struts/DbAware.java
@@ -1,11 +1,9 @@
 package nl.gmt.data.struts;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
+@Inherited
 public @interface DbAware {
 }
