896f3c5a73abbe10e552f72f3501b60287b316c6
Pieter van Ginkel <pginkel@gmt.nl>
2015-06-25 14:22:00 +0200

    Implemented MySQL to Postgres migration tool.
    Implemented a migration tool to transfer a database from MySQL to Postgres based on a schema.

---
+++ mysql2postgres/src/main/java/nl/gmt/data/mysql2postgres/App.java
@@ -0,0 +1,21 @@
+package nl.gmt.data.mysql2postgres;
+
+import nl.gmt.data.DataException;
+import org.kohsuke.args4j.CmdLineException;
+
+public class App {
+    public static void main(String[] args) throws DataException {
+        Arguments arguments;
+        try {
+            arguments = new Arguments(args);
+        } catch (CmdLineException e) {
+            return;
+        }
+
+        try {
+            new Transferer(arguments).transfer();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ mysql2postgres/src/main/java/nl/gmt/data/mysql2postgres/Arguments.java
@@ -0,0 +1,41 @@
+package nl.gmt.data.mysql2postgres;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+
+public class Arguments {
+    @Option(name = "-f", aliases = {"--from"}, required = true, usage = "Source MySQL connection string")
+    private String from;
+
+    @Option(name = "-t", aliases = {"--to"}, required = true, usage = "Destination Postgres connection string")
+    private String to;
+
+    @Option(name = "-s", aliases = {"--schema"}, required = true, usage = "Path to database schema")
+    private String schema;
+
+    public Arguments(String... args) throws CmdLineException {
+        CmdLineParser parser = new CmdLineParser(this, ParserProperties.defaults().withUsageWidth(80));
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+
+            throw e;
+        }
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+}
